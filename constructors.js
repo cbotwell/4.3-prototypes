2f718a1b674f607078e037ab6f610b47ac88df2f
Corbin Otwell <cbotwell@gmail.com>
2015-07-23 08:43:52 -0500

    added email test for .com and .net

--- constructors.js
+++ constructors.js
@@ -189,7 +189,7 @@ console.assert(safe.unlock(rightKey) === sensitive, 'valid keys should open the 
 
 function Validator() {
   this.email = function(email) {
-    if (email.indexOf('@') >= 0) {
+    if (email.indexOf('@') >= 0 && (email.slice(-4) === '.com' || email.slice(-4) === '.net')) {
       return true;
     } else {
       return false;
